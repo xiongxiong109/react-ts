7f6b001fef35dcb25d36bdefbf67ad73d188a181
xjq熊剑桥 <jqxiong@Ctrip.com>
2018-06-29 13:00:36 +0800

    ts

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -5,17 +5,33 @@ interface Iprops {
   age?: number;
 }
 
-function Hello({nm, age}: Iprops) {
-  return (
-    <div>
-      <p>hello, {nm}</p>
-      {
-        age
-        ? <p>aged: {age}</p>
-        : null
-      }
-    </div>
-  )  
+// function Hello({nm, age}: Iprops) {
+//   return (
+//     <div>
+//       <p>hello, {nm}</p>
+//       {
+//         age
+//         ? <p>aged: {age}</p>
+//         : null
+//       }
+//     </div>
+//   )  
+// }
+
+class Hello extends React.Component<Iprops, object> {
+  public render() {
+    const { nm, age } = this.props;
+    return (
+      <div>
+        <p>hello, {nm}</p>
+        {
+          age
+          ? <p>aged: {age}</p>
+          : null
+        }
+      </div>
+    )
+  }
 }
 
 export default Hello
